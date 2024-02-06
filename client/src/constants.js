0473047fa5c208c0b9201824039bd41bc6cfdac0
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-06 23:31:10 +0600

    cng const file

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
+export const BASE_URL = "https://api-secure.vercel.app/api/v1";
 // export const BASE_URL = "http://localhost:9000/api/v1";
