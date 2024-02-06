9fb4af99045c490e966e22aefa7b3532e9a97340
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-06 23:24:27 +0600

    user are got when login , current user

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,36 @@
+const User = require("../models/user.models");
+const JWT = require("jsonwebtoken");
+
+const verifyJWT = async (req, res, next) => {
+  try {
+    const accessToken =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!accessToken) {
+      return res.status(401).json({ message: "Unauthorized request" });
+    }
+
+    const decodedToken = JWT.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id).select(
+      " -password -refreshToken"
+    );
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid access token" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ error: error?.message, message: "Invalid access token" });
+  }
+};
+
+module.exports = verifyJWT;
--- server/router/user.router.js
+++ server/router/user.router.js
@@ -5,12 +5,13 @@ const {
   loginUser,
   getCurrentUser,
 } = require("../controllers/userController");
+const verifyJWT = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
 router.route("/allusers").get(getAllUser);
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/current-user").get(getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 module.exports = router;
