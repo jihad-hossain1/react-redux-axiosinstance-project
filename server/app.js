934ed6a2e43da362d22445bb494c6d287a43c83e
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-07 09:27:13 +0600

    some cng on server cors policy

--- server/app.js
+++ server/app.js
@@ -5,11 +5,14 @@ const cookieParser = require("cookie-parser");
 const app = new express();
 
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
+  cors({ credentials: true, origin: true, exposedHeaders: ["Set-Cookie"] })
 );
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kn" }));
