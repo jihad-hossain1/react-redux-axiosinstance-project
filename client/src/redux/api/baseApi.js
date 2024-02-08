9cc82dceee3f40995e9f98a5142bbc503af1b992
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-08 08:00:35 +0600

    base api added on store on redux

---
+++ client/src/redux/api/baseApi.js
@@ -0,0 +1,8 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000/api/v1" }),
+  endpoints: () => ({}),
+});
+export default baseApi;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "./slice/authSlice";
+import baseApi from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
     auth: authSliceReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
